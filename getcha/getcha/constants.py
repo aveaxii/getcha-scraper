3fb132d17af9e8c4dcdcd79fb355e7d17c9facfc
Kim Alex <kimalex2004dev@gmail.com>
2024-11-15 18:06:58 +0900

    add headers

---
+++ getcha/getcha/constants.py
@@ -0,0 +1,22 @@
+car_list_headers = {
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-US,en;q=0.9,ru;q=0.8",
+    "content-type": "application/json",
+    "origin": "https://used-car.getcha.kr",
+    "priority": "u=1, i",
+    "referer": "https://used-car.getcha.kr/",
+    "sec-ch-ua": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site",
+    "used_car_point_rules_no_in": "10.0",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+    "x-cha-agent": "web",
+    "x-cha-app-version": "8.6.26.292",
+    "x-cha-os": "web",
+    "x-picky-device-id": "1tBllc2vrofw8DrbVcUrEPmnqPDI",
+    "x-picky-session-id": "sid|1731650466278"
+}
