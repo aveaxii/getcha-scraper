d8ecee697a8ee47b90bbd237f994abcfdc16911a
Kim Alex <kimalex2004dev@gmail.com>
2024-11-15 18:35:20 +0900

    add general item filling

--- getcha/getcha/spiders/general.py
+++ getcha/getcha/spiders/general.py
@@ -2,9 +2,10 @@ import scrapy
 import json
 from colorlog_config import logger
 from getcha.constants import car_list_headers
+from getcha.items import GeneralInfoItem
 
-class TestSpider(scrapy.Spider):
-    name = "test"
+class GeneralSpider(scrapy.Spider):
+    name = "general"
     allowed_domains = ["m.getcha.kr", "api.getcha.io"]
 
     car_list_url = 'https://api.getcha.io/api/used-car/sale/filter?page=1&sort=relevant&direction=desc'
@@ -37,5 +38,25 @@ class TestSpider(scrapy.Spider):
         else:
             logger.error("Failed to retrieve data. Status code: %s", response.status)
             
-            
+
+        data = response.json()["result"]["data"]
         
+        for car in data:
+            model_parts = car["modelName"].split()
+            brand = model_parts[0] if len(model_parts) > 0 else ""
+            model = model_parts[1] if len(model_parts) > 1 else ""
+            submodel = " ".join(model_parts[2:]) if len(model_parts) > 2 else ""
+            
+            item = GeneralInfoItem()
+            item["car_id"] = car["id"]
+            item["title"] = car["modelName"]
+            item["distance"] = car["mileage"]
+            item["year"] = car["carYear"]
+            item["price"] = car["price"]
+            item["brand"] = brand
+            item["model"] = model
+            item["submodel"] = submodel
+            item["grade"] = submodel
+            
+            logger.info(f"General info item for car ID {car['id']}: {item}")
+            yield item
